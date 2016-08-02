e451607637e35e314eed8867c1a3ea21509aa955
ylhu <ylhu@bainainfo.com>
2016-08-02 18:58:13 +0800

    update

--- app/src/main/java/cn/ittiger/demo/ui/VideoPlayerControllerView.java
+++ app/src/main/java/cn/ittiger/demo/ui/VideoPlayerControllerView.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -17,13 +18,13 @@ import android.widget.TextView;
  */
 public class VideoPlayerControllerView extends RelativeLayout implements View.OnClickListener {
 
-    public static final String UNKNOWN_SIZE = "N/A";
-    private ImageView mPlayBtn;
+    private static final int HIDE_DELAY = 3000;
+    private static final String UNKNOWN_SIZE = "00:00";
     private ImageView mFullScreenBtn;
     private TextView mPlayTimeText;
     private TextView mTotalTimeText;
     private SeekBar mSeekBar;
-    private SeekBar mViewOnlySeekBar;
+    private ProgressBar mViewOnlyProgressBar;
     private View mControllerBar;
     private int mVideoDuration = 0;
     private VideoControlListener mControlListener;
@@ -49,14 +50,12 @@ public class VideoPlayerControllerView extends RelativeLayout implements View.On
 
         inflate(context, R.layout.video_player_controller_view, this);
         mControllerBar = findViewById(R.id.controller_bar);
-        mPlayBtn = (ImageView) findViewById(R.id.play_btn);
         mFullScreenBtn = (ImageView) findViewById(R.id.full_screen_btn);
         mPlayTimeText = (TextView) findViewById(R.id.play_time);
         mTotalTimeText = (TextView) findViewById(R.id.total_time);
         mSeekBar = (SeekBar) findViewById(R.id.seekbar);
-        mViewOnlySeekBar = (SeekBar) findViewById(R.id.view_only_seekbar);
+        mViewOnlyProgressBar = (ProgressBar) findViewById(R.id.view_only_progressbar);
 
-        mPlayBtn.setOnClickListener(this);
         mFullScreenBtn.setOnClickListener(this);
         mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -64,9 +63,9 @@ public class VideoPlayerControllerView extends RelativeLayout implements View.On
 
                 if (fromUser) {
                     if (mIsViewOnlySeekBarEnable) {
-                        mViewOnlySeekBar.setProgress(progress);
+                        mViewOnlyProgressBar.setProgress(progress);
                     }
-                    int time = progress * mVideoDuration / 100;
+                    int time = progress * mVideoDuration / 100 * 1000;
                     mControlListener.onProgressChanged(time);
                 }
             }
@@ -87,18 +86,10 @@ public class VideoPlayerControllerView extends RelativeLayout implements View.On
     public void onClick(View v) {
 
         switch (v.getId()) {
-            case R.id.play_btn:
-                mControlListener.playSwitch();
-                break;
             case R.id.full_screen_btn:
                 mControlListener.fullScreen();
                 break;
         }
-    }
-
-    public void setPlayState(VideoPlayState playState) {
-
-        mPlayBtn.setImageResource(playState == VideoPlayState.PLAY ? R.drawable.ic_pause : R.drawable.ic_play);
     }
 
     public void setVideoControlListener(VideoControlListener controlListener) {
@@ -108,8 +99,8 @@ public class VideoPlayerControllerView extends RelativeLayout implements View.On
 
     public void setVideoDuration(int videoDuration) {
 
-        mVideoDuration = videoDuration;
-        mTotalTimeText.setText(formatVideoTimeLength(videoDuration));
+        mVideoDuration = videoDuration / 1000;
+        mTotalTimeText.setText(formatVideoTimeLength(mVideoDuration));
     }
 
     public void setIsViewOnlySeekBarEnable(boolean isViewOnlySeekBarEnable) {
@@ -117,13 +108,22 @@ public class VideoPlayerControllerView extends RelativeLayout implements View.On
         mIsViewOnlySeekBarEnable = isViewOnlySeekBarEnable;
     }
 
-    public void setVideoPlayTime(long playTime) {
+    public void setVideoPlayTime(int playTime) {
 
+        playTime = playTime / 1000;
         mPlayTimeText.setText(formatVideoTimeLength(playTime));
         int progress = (int) (playTime * 1.0 / mVideoDuration * 100 + 0.5f);
         mSeekBar.setProgress(progress);
         if(mIsViewOnlySeekBarEnable) {
-            mViewOnlySeekBar.setProgress(progress);
+            mViewOnlyProgressBar.setProgress(progress);
+        }
+    }
+
+    public void setSecondaryProgress(int progress) {
+
+        mSeekBar.setSecondaryProgress(progress);
+        if(mIsViewOnlySeekBarEnable) {
+            mViewOnlyProgressBar.setSecondaryProgress(progress);
         }
     }
 
@@ -136,7 +136,6 @@ public class VideoPlayerControllerView extends RelativeLayout implements View.On
 
         if (mControllerBar.getVisibility() == View.VISIBLE) {
             mControllerBar.clearAnimation();
-            mControllerBar.removeCallbacks(mHideRunnable);
             Animation animation = AnimationUtils.loadAnimation(getContext(),
                     R.anim.option_leave_from_bottom);
             animation.setAnimationListener(new AnimationImp() {
@@ -146,27 +145,35 @@ public class VideoPlayerControllerView extends RelativeLayout implements View.On
                     super.onAnimationEnd(animation);
                     mControllerBar.setVisibility(View.GONE);
                     if(mIsViewOnlySeekBarEnable) {
-                        mViewOnlySeekBar.setVisibility(VISIBLE);
+                        mViewOnlyProgressBar.setVisibility(VISIBLE);
                     }
-                    mControlListener.onHide();
+                    mControlListener.onControllerHide();
                 }
             });
             mControllerBar.startAnimation(animation);
         } else {
             show();
-            mControllerBar.postDelayed(mHideRunnable, 2000);
+            mControllerBar.postDelayed(mHideRunnable, HIDE_DELAY);
         }
     }
 
     public void show() {
 
-        mControllerBar.setVisibility(View.VISIBLE);
-        if(mIsViewOnlySeekBarEnable) {
-            mViewOnlySeekBar.setVisibility(GONE);
-        }
         mControllerBar.clearAnimation();
         Animation animation = AnimationUtils.loadAnimation(getContext(),
                 R.anim.option_entry_from_bottom);
+        animation.setAnimationListener(new AnimationImp() {
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+                super.onAnimationEnd(animation);
+                mControllerBar.setVisibility(View.VISIBLE);
+                if(mIsViewOnlySeekBarEnable) {
+                    mViewOnlyProgressBar.setVisibility(GONE);
+                }
+                mControlListener.onControllerShow();
+            }
+        });
         mControllerBar.startAnimation(animation);
         mControllerBar.removeCallbacks(mHideRunnable);
     }
@@ -209,7 +216,7 @@ public class VideoPlayerControllerView extends RelativeLayout implements View.On
         if(seconds == 0) {
             formatLength = UNKNOWN_SIZE;
         } else if(seconds < 60) {//小于1分钟
-            formatLength = "00:" + seconds;
+            formatLength = "00:" + (seconds < 10 ? "0" + seconds : seconds);
         } else if(seconds < 60 * 60) {//小于1小时
             long sec = seconds % 60;
             long min = seconds / 60;
@@ -228,17 +235,13 @@ public class VideoPlayerControllerView extends RelativeLayout implements View.On
 
     public interface VideoControlListener {
 
-        void onProgressChanged(int time);
-
-        void playSwitch();
+        void onProgressChanged(int seekTime);
 
         void fullScreen();
 
-        void onHide();
-    }
+        void onControllerShow();
 
-    public enum VideoPlayState {
-        PLAY, PAUSE
+        void onControllerHide();
     }
 }
 
--- app/src/main/java/cn/ittiger/demo/ui/VideoPlayerView.java
+++ app/src/main/java/cn/ittiger/demo/ui/VideoPlayerView.java
@@ -7,12 +7,14 @@ import android.graphics.SurfaceTexture;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 /**
  * Created by baina on 16-7-29.
@@ -20,16 +22,18 @@ import android.widget.RelativeLayout;
 public class VideoPlayerView extends RelativeLayout implements View.OnClickListener,
         VideoPlayerControllerView.VideoControlListener, TextureView.SurfaceTextureListener {
 
-    private TextureView mTextureView;
-    private VideoPlayerControllerView mVideoPlayerControllerView;
-    private ImageView mVideoImage;
-    private ImageView mPlayImage;
-    private ProgressBar mProgressBar;
-    private VideoPlayerControllerView.VideoPlayState mVideoPlayState = VideoPlayerControllerView.VideoPlayState.PAUSE;
+    private TextureView mTextureView;//视频播放容器
+    private VideoPlayerControllerView mVideoPlayerControllerView;//视频底部的播放控制器
+    private ImageView mVideoImage;//视频预览图
+    private ImageView mPlayBtn;//播放屏幕上的暂停按钮
+    private ProgressBar mProgressBar;//视频加载过程中的进度条
+    private VideoPlayState mVideoPlayState = VideoPlayState.PLAY;//视频播放的当前状态：播放，暂停
 
     private Surface mSurface = null;
     private MediaPlayer mPlayer;
     private String mVideoUrl = "http://flv.bn.netease.com/tvmrepo/2012/7/C/7/E868IGRC7-mobile.mp4";
+
+    private int mSecProgress = 0;
 
     public VideoPlayerView(Context context) {
 
@@ -52,12 +56,13 @@ public class VideoPlayerView extends RelativeLayout implements View.OnClickListe
         inflate(context, R.layout.video_player_view_layout, this);
         mTextureView = (TextureView) findViewById(R.id.video_view);
         mVideoImage = (ImageView) findViewById(R.id.video_image);
-        mPlayImage = (ImageView) findViewById(R.id.play_image);
+        mPlayBtn = (ImageView) findViewById(R.id.play_image);
         mVideoPlayerControllerView = (VideoPlayerControllerView) findViewById(R.id.videoControllerView);
         mProgressBar = (ProgressBar) findViewById(R.id.video_loading);
 
         mTextureView.setOnClickListener(this);
-        mPlayImage.setOnClickListener(this);
+        mTextureView.setSurfaceTextureListener(this);
+        mPlayBtn.setOnClickListener(this);
         mVideoPlayerControllerView.setVideoControlListener(this);
     }
 
@@ -65,110 +70,204 @@ public class VideoPlayerView extends RelativeLayout implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.video_view:
-                if(!mVideoPlayerControllerView.isShown()) {
-                    showOrHidePlayBtn();
-                }
                 mVideoPlayerControllerView.showOrHide();
                 break;
             case R.id.play_image:
-                if(mVideoPlayState == VideoPlayerControllerView.VideoPlayState.PAUSE) {
-                    startPlay();
-                } else {
-                    pausePlay();
-                }
-                setCurrentPlayImage();
+                playOrPause();
                 break;
         }
     }
 
-    private void showOrHidePlayBtn() {
+    /**
+     * 播放或暂停播放控制
+     */
+    private void playOrPause() {
 
-        if(mPlayImage.getVisibility() == VISIBLE) {
-            mPlayImage.setVisibility(GONE);
-        } else {
-            mPlayImage.setVisibility(VISIBLE);
-            setCurrentPlayImage();
+        if(loadAndPlay(mVideoUrl)) {
+            if(mVideoPlayState == VideoPlayState.PAUSE) {//当前处于暂停播放状态
+                startPlay();
+            } else {
+                pausePlay();
+            }
+            updatePlayBtnIcon();//切换播放时更新播放按钮的图标
         }
     }
 
-    private void setCurrentPlayImage() {
+    /**
+     * 显示或者隐藏播放暂停按钮
+     */
+    private void showOrHidePlayBtn() {
 
-        int resId = mVideoPlayState == VideoPlayerControllerView.VideoPlayState.PAUSE ? R.drawable.ic_play : R.drawable.ic_pause;
-        mPlayImage.setImageResource(resId);
+        if(mPlayBtn.getVisibility() == VISIBLE) {
+            mPlayBtn.setVisibility(GONE);
+        } else {
+            mPlayBtn.setVisibility(VISIBLE);
+            updatePlayBtnIcon();
+        }
     }
 
-    public void play(String videoUrl) {
+    /**
+     * 更新播放按钮的图标
+     */
+    private void updatePlayBtnIcon() {
+
+        int resId = mVideoPlayState == VideoPlayState.PAUSE ? R.drawable.ic_play : R.drawable.ic_bar_pause;
+        mPlayBtn.setImageResource(resId);
+    }
+
+    /**
+     * 显示加载进度条
+     */
+    private void showLoadingBarIfNeed() {
+
+        if(mProgressBar.getVisibility() == GONE) {
+            mProgressBar.setVisibility(VISIBLE);
+        }
+    }
+
+    /**
+     * 隐藏加载加载进度条
+     */
+    private void hideLoadingBarIfNeed() {
+
+        if(mProgressBar.getVisibility() == VISIBLE) {
+            mProgressBar.setVisibility(GONE);
+        }
+    }
+
+    /**
+     * 加载播放指定URL的视频
+     * @param videoUrl
+     * @return
+     */
+    public boolean loadAndPlay(String videoUrl) {
+
+        if(mPlayer != null) {
+            return true;
+        }
+        showOrHidePlayBtn();
+        showLoadingBarIfNeed();
+        mPlayer = new MediaPlayer();
+        playPrepare(videoUrl);
+        return false;
+    }
+
+    /**
+     * 播放准备
+     * @param videoUrl
+     */
+    private void playPrepare(String videoUrl) {
 
         try {
-            mPlayer = new MediaPlayer();
+            if(mPlayer != null) {
 //            mPlayer.setOnCompletionListener(mOnCompletionListener);//播放完成的监听
-            mPlayer.setOnPreparedListener(mOnPreparedListener);
+                mPlayer.setOnPreparedListener(mOnPreparedListener);
 //            mPlayer.setOnInfoListener(mInfoListener);
-//            mPlayer.setOnErrorListener(mErrorListener);
-//            mPlayer.setOnBufferingUpdateListener(mBufferingUpdateListener);
-            mPlayer.setSurface(mSurface);
-            mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-            mPlayer.setScreenOnWhilePlaying(true);
-            mPlayer.setDataSource(videoUrl);
-            mPlayer.prepareAsync();
+                mPlayer.setOnErrorListener(mErrorListener);
+                mPlayer.setOnBufferingUpdateListener(mBufferingUpdateListener);//缓冲监听
+                mPlayer.setSurface(mSurface);
+                mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                mPlayer.setScreenOnWhilePlaying(true);
+                mPlayer.setDataSource(videoUrl);
+                mPlayer.prepareAsync();
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private MediaPlayer.OnPreparedListener mOnPreparedListener = new MediaPlayer.OnPreparedListener() {
-        @Override
-        public void onPrepared(MediaPlayer mp) {
+    /**
+     * 开始播放
+     */
+    public void startPlay() {
 
-            startPlay();
+        mPlayer.start();//开始播放
+        if(mVideoImage.getVisibility() == VISIBLE) {//播放时隐藏视频图片
+            mVideoImage.setVisibility(GONE);
         }
-    };
+        mVideoPlayState = VideoPlayState.PLAY;//更新当前状态为：播放
+        hideLoadingBarIfNeed();
+    }
 
+    /**
+     * 更新播放进度
+     */
+    private void updatePlayProgress() {
+
+        if(mVideoPlayState == VideoPlayState.PLAY) {
+            int duration = mPlayer.getDuration();
+            int playTime = mPlayer.getCurrentPosition();
+            mVideoPlayerControllerView.setVideoDuration(duration);
+            mVideoPlayerControllerView.setVideoPlayTime(playTime);
+            mVideoPlayerControllerView.setSecondaryProgress(mSecProgress);
+        }
+        postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+
+                updatePlayProgress();
+            }
+        }, 1000);
+    }
+
+    /**
+     * 停止播放
+     */
     public void pausePlay() {
 
         mPlayer.pause();
-        mVideoPlayerControllerView.setPlayState(VideoPlayerControllerView.VideoPlayState.PAUSE);
-        mVideoPlayState = VideoPlayerControllerView.VideoPlayState.PAUSE;
+        mVideoPlayState = VideoPlayState.PAUSE;//更新当前状态为：暂停
     }
 
-    public void startPlay() {
-
-        if(mPlayer == null) {
-            play(mVideoUrl);
-        } else {
-            mPlayer.start();
-            mVideoPlayerControllerView.setPlayState(VideoPlayerControllerView.VideoPlayState.PLAY);
-            mVideoPlayState = VideoPlayerControllerView.VideoPlayState.PLAY;
-        }
-    }
-
+    /**
+     * 拖动播放进度条时的响应
+     *
+     * @param seekTime
+     */
     @Override
-    public void onProgressChanged(int time) {
+    public void onProgressChanged(int seekTime) {
 
+        mPlayer.seekTo(seekTime);
     }
 
-    @Override
-    public void playSwitch() {
-
-
-    }
-
+    /**
+     * 底部播放控制器上的全屏按钮回调
+     */
     @Override
     public void fullScreen() {
 
+        Toast.makeText(getContext(), "点击全屏", Toast.LENGTH_SHORT).show();
     }
 
+    /**
+     * 底部播放控制器隐藏时的回调
+     */
     @Override
-    public void onHide() {
+    public void onControllerHide() {
 
-        mPlayImage.setVisibility(GONE);
+        if(mPlayBtn.getVisibility() == VISIBLE) {
+            mPlayBtn.setVisibility(GONE);
+        }
+    }
+
+    /**
+     * 底部播放控制器显示时的回调
+     */
+    @Override
+    public void onControllerShow() {
+
+        if(mPlayBtn.getVisibility() == GONE) {
+            mPlayBtn.setVisibility(VISIBLE);
+        }
+        updatePlayBtnIcon();
     }
 
     @Override
     public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int width, int height) {
 
         mSurface = new Surface(surfaceTexture);
-        play(mVideoUrl);
+        playPrepare(mVideoUrl);
     }
 
     @Override
@@ -185,5 +284,44 @@ public class VideoPlayerView extends RelativeLayout implements View.OnClickListe
     @Override
     public void onSurfaceTextureUpdated(SurfaceTexture surface) {
 
+    }
+
+    private MediaPlayer.OnPreparedListener mOnPreparedListener = new MediaPlayer.OnPreparedListener() {
+        @Override
+        public void onPrepared(MediaPlayer mp) {
+
+            //视频初始完成，即将播放，初始化完成后显示播放控制其
+            mVideoPlayerControllerView.showOrHide();
+            updatePlayProgress();
+            startPlay();
+        }
+    };
+
+    private MediaPlayer.OnErrorListener mErrorListener = new MediaPlayer.OnErrorListener() {
+
+        @Override
+        public boolean onError(MediaPlayer mp, int what, int extra) {
+            if (getWindowToken() != null) {
+                String message = "播放错误";
+
+                Log.e("mErrorListener", message);
+            }
+            return true;
+        }
+    };
+
+    private MediaPlayer.OnBufferingUpdateListener mBufferingUpdateListener = new MediaPlayer.OnBufferingUpdateListener() {
+
+        @Override
+        public void onBufferingUpdate(MediaPlayer mp, int percent) {
+
+            mSecProgress = percent;
+        }
+    };
+
+
+    public enum VideoPlayState {
+
+        PLAY, PAUSE
     }
 }
