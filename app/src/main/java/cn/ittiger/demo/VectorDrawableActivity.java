22872c7f7df5a3bab2554069be101803368b424e
huyongli <huyongli1989@gmail.com>
2016-11-13 21:35:16 +0800

    update

--- app/src/main/java/cn/ittiger/demo/VectorDrawableActivity.java
+++ app/src/main/java/cn/ittiger/demo/VectorDrawableActivity.java
@@ -3,10 +3,19 @@ package cn.ittiger.demo;
 import android.support.graphics.drawable.AnimatedVectorDrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
 import android.widget.ImageView;
 
 public class VectorDrawableActivity extends AppCompatActivity {
-
+    static {
+        /**
+         * 此方法必须必须引用appcompat-v7:23.4.0
+         *
+         * Button类控件使用vector必须使用selector进行包装才会起作用，不然会crash
+         * 并且使用selector时必须调用下面的方法进行设置，否则也会crash
+         * */
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +24,6 @@ public class VectorDrawableActivity extends AppCompatActivity {
         setContentView(R.layout.activity_vector_drawable);
 
         ImageView imageView = (ImageView) findViewById(R.id.imageView);
-//        AnimatedVectorDrawableCompat morphing = (AnimatedVectorDrawableCompat) getResources().getDrawable(R.drawable.bot_animate);
         AnimatedVectorDrawableCompat animatedVectorDrawableCompat = AnimatedVectorDrawableCompat.create(this, R.drawable.rebot_animate);
         imageView.setImageDrawable(animatedVectorDrawableCompat);
         if (animatedVectorDrawableCompat != null) {
@@ -23,11 +31,18 @@ public class VectorDrawableActivity extends AppCompatActivity {
         }
 
         ImageView star = (ImageView) findViewById(R.id.starImageView);
-//        AnimatedVectorDrawableCompat morphing = (AnimatedVectorDrawableCompat) getResources().getDrawable(R.drawable.bot_animate);
         AnimatedVectorDrawableCompat starDrawable = AnimatedVectorDrawableCompat.create(this, R.drawable.five_star_animate);
         star.setImageDrawable(starDrawable);
         if (starDrawable != null) {
             starDrawable.start();
+        }
+
+        ImageView load = (ImageView) findViewById(R.id.loadingImageView);
+//        AnimatedVectorDrawable loadDrawable = (AnimatedVectorDrawable) getResources().getDrawable(R.drawable.loading_animate);
+        AnimatedVectorDrawableCompat loadDrawable = AnimatedVectorDrawableCompat.create(this, R.drawable.loading_animate);
+        load.setImageDrawable(loadDrawable);
+        if (loadDrawable != null) {
+            loadDrawable.start();
         }
     }
 }
